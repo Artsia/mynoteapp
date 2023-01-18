195b177fe72c1b4b70993d006b541d2320fbd6e0
phillip <65820314+Artsia@users.noreply.github.com>
2023-01-18 12:13:48 -0500

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let commentContainer = document.getElementsByClassName("commentContainer");
+let redDot = document.getElementsByClassName("redDot");
+let markAllBtn = document.getElementsById("markBtn");
+
+
+
+function changeColor(){
+    redDot.style.color = "white";
+    commentContainer.style.color = "blue";
+}
